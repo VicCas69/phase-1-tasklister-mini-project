fea5d43632b4814a69bfbf777b66fe8c7a8d91b5
victor castro <victor_castro_01@hotmail.com>
2021-09-08 21:34:44 -0400

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const f = document.querySelector('form')
+  f.addEventListener('submit',(event)=>{
+    event.preventDefault()
+    creatingList(event.target.new_task_description.value)
+    
+
+  })
+})
+
+function creatingList(task){
+  const p = document.createElement('p')
+  const btn = document.createElement('button')
+  btn.textContent = 'X'
+  btn.addEventListener('click', handleDelete)
+  p.textContent = task
+  p.appendChild(btn)
+  document.getElementById("tasks").appendChild(p)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
+
+
